6c9b4d5ad4e1e18ffabc4d0997bba34effc8a8f8
Ursu Adrian <uursu.adrian@gmail.com>
2022-01-17 20:30:15 +0200

    newProject strip item refactored || TO DO (preview-rectangle/ellipse)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,13 +38,16 @@
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.freeDraw = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -118,10 +121,13 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
             this.panel1.Controls.Add(this.button5);
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.freeDraw);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 33);
@@ -129,9 +135,49 @@
             this.panel1.Size = new System.Drawing.Size(200, 827);
             this.panel1.TabIndex = 4;
             // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(0, 554);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(96, 60);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Rectangle";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(-1, 468);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(97, 69);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Elipse";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(-1, 381);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 81);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Rotate 90\' Left";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(-1, 278);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 81);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Rotate 90\' Right";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(3, 215);
+            this.button3.Location = new System.Drawing.Point(0, 173);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(96, 81);
             this.button3.TabIndex = 2;
@@ -139,15 +185,15 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button2
+            // freeDraw
             // 
-            this.button2.Location = new System.Drawing.Point(3, 138);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 50);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Draw";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.freeDraw.Location = new System.Drawing.Point(0, 107);
+            this.freeDraw.Name = "freeDraw";
+            this.freeDraw.Size = new System.Drawing.Size(96, 60);
+            this.freeDraw.TabIndex = 1;
+            this.freeDraw.Text = "Free Draw";
+            this.freeDraw.UseVisualStyleBackColor = true;
+            this.freeDraw.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -170,29 +216,20 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
-            // button4
+            // button2
             // 
-            this.button4.Location = new System.Drawing.Point(12, 343);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(97, 81);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Rotate 90\' Right";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(12, 447);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(97, 81);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Rotate 90\' Left";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button2.Location = new System.Drawing.Point(105, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 62);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Undo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // MyPaint
             // 
@@ -228,11 +265,14 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button freeDraw;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,9 +9,15 @@ namespace MyPaint
     {
         Bitmap bitmap;
 
-        Point lastPoint;
+        Point startPoint;
+        Point finishPoint;
+        Rectangle rect;
+
         bool isPenDown;
-        bool isPenActive = false;
+        bool isFreeDrawActive = false;
+        bool isElipseActive = false;
+        bool isRectangleActive = false;
+
 
         public MyPaint()
         {
@@ -59,8 +65,15 @@ namespace MyPaint
 
         private void newProjectToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            pictureBox1.Image.Dispose();
-            pictureBox1.Image = null;
+            if(pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = null;
+            }
+            else
+            {
+                this.Refresh();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -92,45 +105,97 @@ namespace MyPaint
 
         private void button2_Click(object sender, EventArgs e)
         {
-            isPenActive = true;
+            isFreeDrawActive = true;
+            isRectangleActive = false;
+            isElipseActive = false;
         }
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            lastPoint = e.Location; 
+            startPoint = e.Location; 
             isPenDown = true;
+
+            
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
+            finishPoint = e.Location;
             Graphics graphics = pictureBox1.CreateGraphics();
-            Point point = e.Location;
+
             Pen pen = new Pen(colorDialog1.Color);
 
-            if (isPenDown && isPenActive)
-            {
-                if (lastPoint != null)
-                {
-                    graphics.DrawLine(pen, lastPoint, point);
-                }
-                lastPoint = point;
-            }
-        }
 
+            if (isPenDown && isFreeDrawActive)
+            {
+                if (startPoint != null)
+                {
+                    graphics.DrawLine(pen, startPoint, finishPoint);
+                }
+                startPoint = finishPoint;
+            }
+          //else if (isRectangleActive && isPenDown)
+          //{
+          //   graphics.DrawRectangle(pen, Math.Min(startPoint.X, finishPoint.X), Math.Min(startPoint.Y, finishPoint.Y), Math.Abs(finishPoint.X - startPoint.X), Math.Abs(finishPoint.Y - startPoint.Y));
+          //  }
+        }
+      
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
             isPenDown = false;
+            finishPoint = e.Location;
+
+            Graphics graphics = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(colorDialog1.Color);
+
+            if (isRectangleActive)
+            {
+                graphics.DrawRectangle(pen, Math.Min(startPoint.X, finishPoint.X), Math.Min(startPoint.Y, finishPoint.Y), Math.Abs(finishPoint.X - startPoint.X), Math.Abs(finishPoint.Y - startPoint.Y));
+
+            }
+            else if (isElipseActive)
+            {
+                graphics.DrawEllipse(pen, Math.Min(startPoint.X, finishPoint.X), Math.Min(startPoint.Y, finishPoint.Y), Math.Abs(finishPoint.X - startPoint.X), Math.Abs(finishPoint.Y - startPoint.Y));
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            isElipseActive = true;
+            isFreeDrawActive = false;
+            isRectangleActive = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            isRectangleActive = true;
+            isElipseActive = false;
+            isFreeDrawActive = false;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
             {
                 Color color = colorDialog1.Color;
 
             }
         }
 
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+           // pictureBox1.Invalidate();   
+        }
 
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            //Pen pen = new Pen(colorDialog1.Color);
+
+            //if (isRectangleActive)
+            //{
+             //   e.Graphics.DrawRectangle(pen, Math.Min(startPoint.X, finishPoint.X), Math.Min(startPoint.Y, finishPoint.Y), Math.Abs(finishPoint.X - startPoint.X), Math.Abs(finishPoint.Y - startPoint.Y));
+           // }
+        }
     }
 }
